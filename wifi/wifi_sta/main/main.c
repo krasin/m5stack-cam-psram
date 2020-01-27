9f757e8bf840cb6823903a64757e9ab9d6c9df43
Ivan Krasin <krasin@robodone.com>
2020-01-26 18:26:55 -0800

    Remove support of streaming mode.
    It's not needed + it's broken by the previous commit.

--- wifi/wifi_sta/main/main.c
+++ wifi/wifi_sta/main/main.c
@@ -26,11 +26,6 @@ static const char* TAG = "camera";
 
 #define MAX_STA_CONN  1
 
-#define PART_BOUNDARY "123456789000000000000987654321"
-static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
-static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
-
 static EventGroupHandle_t s_wifi_event_group;
 static esp_ip4_addr_t s_ip_addr;
 const int CONNECTED_BIT = BIT0;
@@ -155,71 +150,6 @@ esp_err_t jpg_httpd_handler(httpd_req_t *req){
     return res;
 }
 
-esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
-    camera_fb_t * fb = NULL;
-    esp_err_t res = ESP_OK;
-    size_t _jpg_buf_len;
-    uint8_t * _jpg_buf;
-    char * part_buf[64];
-    static int64_t last_frame = 0;
-    if(!last_frame) {
-        last_frame = esp_timer_get_time();
-    }
-
-    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
-    if(res != ESP_OK){
-        return res;
-    }
-
-    while(true){
-        fb = esp_camera_fb_get();
-        if (!fb) {
-            ESP_LOGE(TAG, "Camera capture failed");
-            res = ESP_FAIL;
-        } else {
-            if(fb->format != PIXFORMAT_JPEG){
-                bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
-                if(!jpeg_converted){
-                    ESP_LOGE(TAG, "JPEG compression failed");
-                    esp_camera_fb_return(fb);
-                    res = ESP_FAIL;
-                }
-            } else {
-                _jpg_buf_len = fb->len;
-                _jpg_buf = fb->buf;
-            }
-        }
-        if(res == ESP_OK){
-            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
-
-            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
-        }
-        if(res == ESP_OK){
-            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
-        }
-        if(res == ESP_OK){
-            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
-        }
-        if(fb->format != PIXFORMAT_JPEG){
-            free(_jpg_buf);
-        }
-        esp_camera_fb_return(fb);
-        if(res != ESP_OK){
-            break;
-        }
-        int64_t fr_end = esp_timer_get_time();
-        int64_t frame_time = fr_end - last_frame;
-        last_frame = fr_end;
-        frame_time /= 1000;
-        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
-            (uint32_t)(_jpg_buf_len/1024),
-            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
-    }
-
-    last_frame = 0;
-    return res;
-}
-
 static esp_err_t http_server_init(){
     httpd_handle_t server;
     httpd_uri_t jpeg_uri = {
@@ -229,18 +159,10 @@ static esp_err_t http_server_init(){
         .user_ctx = NULL
     };
 
-    httpd_uri_t jpeg_stream_uri = {
-        .uri = "/",
-        .method = HTTP_GET,
-        .handler = jpg_stream_httpd_handler,
-        .user_ctx = NULL
-    };
-
     httpd_config_t http_options = HTTPD_DEFAULT_CONFIG();
 
     ESP_ERROR_CHECK(httpd_start(&server, &http_options));
     ESP_ERROR_CHECK(httpd_register_uri_handler(server, &jpeg_uri));
-    ESP_ERROR_CHECK(httpd_register_uri_handler(server, &jpeg_stream_uri));
 
     return ESP_OK;
 }
